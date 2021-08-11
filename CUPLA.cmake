69a77cf8c9a0bf715fef7462b43d68a57827a074
Jeff Young <jyoung9@gatech.edu>
2021-08-11 13:47:52 -0400

    Cleaned up CUPLA.cmake

--- CUPLA.cmake
+++ CUPLA.cmake
@@ -23,23 +23,11 @@ macro(setup)
     list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake/Modules")
     find_package(cupla REQUIRED)
 
-    #enable_language(CUPLA)
-
     #Point to directory that contains cuda_to_cupla.h in your CUPLA build to avoid compilation errors
     set(CUPLA_INCLUDE_DIR ${cupla_ROOT}/include)
     #Also add the Alpaka and Boost includes - note that Boost 1.65 or greater is required
     set(ALPAKA_INCLUDE_DIR ${cupla_ROOT}/alpaka/include)
     set(BOOST_INCLUDE_DIR "$ENV{BOOST_ROOT}/include")
     include_directories(${CUPLA_INCLUDE_DIR} ${ALPAKA_INCLUDE_DIR} ${BOOST_INCLUDE_DIR})
-    #set(CUPLA_HEADERS ${CUPLA_ROOT}/*)
-
-    # add -forward-unknown-to-host-compiler for compatibility reasons
-    set(CMAKE_CUPLA_FLAGS ${CMAKE_CUPLA_FLAGS} "" ${CUPLA_EXTRA_FLAGS})
-
-    # CMake defaults to -O2 for CUDA at Release, let's wipe that and use the global RELEASE_FLAG
-    # appended later
-    wipe_gcc_style_optimisation_flags(CMAKE_CUPLA_FLAGS_${BUILD_TYPE})
-   
-    message(STATUS "CUPLA flags: ${CMAKE_CUPLA_FLAGS} ${CMAKE_CUPLA_FLAGS_${BUILD_TYPE}}")
 
 endmacro()
